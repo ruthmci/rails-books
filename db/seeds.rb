0a623c9df281419f8a33a077b21dbf3d59acc1f6
Ruth <ruthmcilwaine@gmail.com>
2019-04-15 13:00:33 +1000

    book model setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Start of Seeding..."
+Book.destroy_all
+50.times do
+  params = {
+    title: Faker::Book.unique.title,
+    author: Faker::Book.unique.author,
+    publisher: Faker::Book.publisher,
+    genre: Faker::Book.genre,
+    description: Faker::Lorem.paragraph  
+  }
+  puts "Creating Book: #{params}"
+  book = Book.new(params)
+  book.save
+end
+
+puts "Seeding Over"
