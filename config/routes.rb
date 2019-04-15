4a2e8217731d2575f37c9f24bada0ec4347890b2
Ruth <ruthmcilwaine@gmail.com>
2019-04-15 14:42:31 +1000

    list and detail view wired up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'books#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books/:id', to: 'books#show'
 end
