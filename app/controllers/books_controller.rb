b23ef9f895fb5b8c8f88a0bf000df8528c0f99d3
Ruth <ruthmcilwaine@gmail.com>
2019-04-17 11:18:21 +1000

    finished CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,40 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.publisher = params[:publisher]
+    @book.genre = params[:genre]
+    @book.description = params[:description]
+    @book.save
+    redirect_to root_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.publisher = params[:publisher]
+    @book.genre = params[:genre]
+    @book.description = params[:description]
+    @book.save
+    redirect_to root_path
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'books#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'books#index', as: 'books'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  post '/books/new', to: 'books#create'
   get '/books/:id', to: 'books#show'
+  post '/books/:id', to: 'books#delete', as: 'delete_book'
+  get '/books/edit/:id', to: 'books#edit', as: 'edit_book'
+  put '/books/edit/:id', to: 'books#update'
+  # post '/books/update/:id', to: 'books#update'
+
 end
